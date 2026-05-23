cb66582a72145208d7eb3f44d67b06aca31ae420
CarnageOG <egetashviligiorgi9@gmail.com>
2026-05-23 11:17:28 +0400

    made home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,55 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      hello world
+      <div className={styles.links}>
+        <Link href="/login" className={styles.card}>
+          <h2>Login Page</h2>
+          <Image
+            className={styles.image}
+            src="/login.png"
+            alt="login"
+            width={400}
+            height={200}
+          />
+        </Link>
+
+        <Link href="/products" className={styles.card}>
+          <h2>Product Page</h2>
+          <Image
+            className={styles.image}
+            src="/product.png"
+            alt="products"
+            width={400}
+            height={200}
+          />
+        </Link>
+
+        <Link href="/profile" className={styles.card}>
+          <h2>Profile Page</h2>
+          <Image
+            className={styles.image}
+            src="/profile.png"
+            alt="profile"
+            width={400}
+            height={200}
+          />
+        </Link>
+
+        <Link href="/cart" className={styles.card}>
+          <h2>Cart Page</h2>
+          <Image
+            className={styles.image}
+            src="/cart.png"
+            alt="cart"
+            width={400}
+            height={200}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
